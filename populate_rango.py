07b8010a3b799384741435a5d237113f6babc7fc
Anwar Abdullah <2666539a@student.gla.ac.uk>
2023-02-01 11:16:54 +0000

    Completed Chapter 6(Last commit was accidently labelled as completed chapter 4, it should have been Chapter 5)

--- populate_rango.py
+++ populate_rango.py
@@ -6,24 +6,24 @@ django.setup()
 from rango.models import Category, Page
 
 def populate():
-    python_pages = [{'title': 'Official Python Tutorial','url':'http://docs.python.org/3/tutorial/'},
-                    {'title':'How to Think like a Computer Scientist','url':'http://www.greenteapress.com/thinkpython/'},
-                    {'title': 'Learn Python in 10 Minutes','url':'http://www.korokithakis.net/tutorials/python/'}]
+    python_pages = [{'title': 'Official Python Tutorial','url':'http://docs.python.org/3/tutorial/','views':'4'},
+                    {'title':'How to Think like a Computer Scientist','url':'http://www.greenteapress.com/thinkpython/','views':'10'},
+                    {'title': 'Learn Python in 10 Minutes','url':'http://www.korokithakis.net/tutorials/python/','views':'2'}]
 
 
-    django_pages = [{'title': 'Official Django Tutorial','url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-                    {'title': 'Django Rocks','url': 'http://www.djangorocks.com/'},
-                    {'title': 'How to Tango with Django','url': 'http://www.tangowithdjango.com/'}]
+    django_pages = [{'title': 'Official Django Tutorial','url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/','views':'90'},
+                    {'title': 'Django Rocks','url': 'http://www.djangorocks.com/','views':'13'},
+                    {'title': 'How to Tango with Django','url': 'http://www.tangowithdjango.com/','views':'10'}]
 
-    other_pages = [{'title': 'Bottle','url':'http://bottlepy.org/docs/dev/'},
-                    {'title': 'Flask','url': 'http://flask.pocoo.org'}]
+    other_pages = [{'title': 'Bottle','url':'http://bottlepy.org/docs/dev/','views':'8'},
+                    {'title': 'Flask','url': 'http://flask.pocoo.org','views':'1'}]
 
     cats = {'Python': {'pages': python_pages,'views':128,'likes':64},'Django': {'pages': django_pages,'views':64,'likes':32},'Other Frameworks': {'pages': other_pages,'views':32,'likes':16}}
 
     for cat, cat_data in cats.items():
         c = add_cat(cat,cat_data['views'],cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'],p['views'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
--- rango/admin.py
+++ rango/admin.py
@@ -8,7 +8,9 @@ class PageAdmin(admin.ModelAdmin):
 
     list_display = ('title','category','url')
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
